fdb0cea551a186c72f8da75294721326e80a0f5a
congdaivu107 <congdaivu107@gmail.com>
2024-10-13 03:32:31 +0700

    first commit

---
+++ VuCongDai_Lab6/App.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+import React from "react";
+import ChatScreen from "./components/4_a";
+
+const App = () => {
+  return <ChatScreen></ChatScreen>;
+};
+
+export default App;
