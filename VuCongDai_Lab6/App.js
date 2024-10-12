92293c06e83cace70764baa59a34bc340a765f3f
congdaivu107 <congdaivu107@gmail.com>
2024-10-13 04:03:47 +0700

    update code screen 4_b

--- VuCongDai_Lab6/App.js
+++ VuCongDai_Lab6/App.js
@@ -1,9 +1,10 @@
 import { View, Text } from "react-native";
 import React from "react";
 import ChatScreen from "./components/4_a";
+import Screen2 from "./components/4_b";
 
 const App = () => {
-  return <ChatScreen></ChatScreen>;
+  return <Screen2></Screen2>;
 };
 
 export default App;
